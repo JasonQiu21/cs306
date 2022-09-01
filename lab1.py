5f93fc5e945f5e44e96a2e44e5caa757ad61f668
Jason Qiu <jqiu21@protonmail.com>
2022-09-01 19:30:30 -0400

    Add lab 1

---
+++ lab1.py
@@ -0,0 +1,29 @@
+# I pledge my honor that I have abided by the Stevens Honor System.
+
+# Jason Qiu
+# 2022-09-01
+
+def extended_euclid_fronthalf(a, b):
+    if b%a:
+        print(f"{b%a} =  {b} - {b//a}*{a}")
+        extended_euclid_fronthalf(b%a, a)
+
+
+def extended_euclid(a, b):
+    """Implementation of gcd using Euler's Extended Algorithm. 
+    Returns GCD g coefficients x and y of linear combination xa + yb"""
+    if a == 0:
+        return (b, 0, 1)
+    else:
+        g, y, x = extended_euclid(b % a, a)
+        if y:
+            print(f"{g} = {x - (b // a) * y}*{a} + {y}*{b}")
+        return (g, x - (b // a) * y, y)
+
+def gcd(a, b):
+    g, x, y = extended_euclid(a, b)
+    return g
+
+extended_euclid_fronthalf(21, 30)
+print("\n")
+print(gcd(21, 30))
