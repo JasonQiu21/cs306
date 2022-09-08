e1d6dbef7df2388322d9e893e756add55efbd1fa
Jason Qiu <jqiu21@protonmail.com>
2022-09-08 16:41:58 -0400

    Add no memoization version

--- lab2.py
+++ lab2.py
@@ -14,5 +14,15 @@ def fast_pow(a, b, m):
         memo[(a, b, m)] = (fast_pow(a, b//2, m) * fast_pow(a, b//2, m))%m
         return memo[(a, b, m)]
 
+def no_memo_pow(a, b, m):
+    """Compute a^b mod m"""
+    if b == 1:
+        return a%m
+    elif b%2:
+        return (a%m * fast_pow(a, b//2, m) * fast_pow(a, b//2, m))%m
+    else:
+        return (fast_pow(a, b//2, m) * fast_pow(a, b//2, m))%m
+
+print(no_memo_pow(20, 2001, 33))
 print(fast_pow(20, 2001, 33))
 print(pow(20, 2001, 33))
