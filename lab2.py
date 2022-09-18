9758c679db103580d4d744d575d666427a63b78a
Jason Qiu <jqiu21@protonmail.com>
2022-09-18 10:23:39 -0400

    Correct error in lab3, add timing to lab3

--- lab2.py
+++ lab2.py
@@ -1,3 +1,8 @@
+# I pledge my honor that I have abided by the Stevens Honor System.
+
+# Jason Qiu
+# 2022-09-21
+
 memo = {}
 
 def fast_pow(a, b, m):
@@ -14,14 +19,15 @@ def fast_pow(a, b, m):
         memo[(a, b, m)] = (fast_pow(a, b//2, m) * fast_pow(a, b//2, m))%m
         return memo[(a, b, m)]
 
+# Version with no memoization
 def no_memo_pow(a, b, m):
     """Compute a^b mod m"""
     if b == 1:
         return a%m
     elif b%2:
-        return (a%m * fast_pow(a, b//2, m) * fast_pow(a, b//2, m))%m
+        return (a%m * no_memo_pow(a, b//2, m) * no_memo_pow(a, b//2, m))%m
     else:
-        return (fast_pow(a, b//2, m) * fast_pow(a, b//2, m))%m
+        return (no_memo_pow(a, b//2, m) * no_memo_pow(a, b//2, m))%m
 
 if __name__ == '__main__':
     print(no_memo_pow(20, 2001, 33))
--- lab3.py
+++ lab3.py
@@ -1,16 +1,38 @@
-from lab2 import no_memo_pow
+# I pledge my honor that I have abided by the Stevens Honor System.
+
+# Jason Qiu
+# 2022-09-21
+
+from lab2 import no_memo_pow, fast_pow # Divide-and-conquer algorithm for modular exponentiation (implemented last week)
+from random import randint
+from time import time_ns
 
 def faster_pow(a, b, m):
     """Compute a^b mod m"""
     result = 1
     prev = None
-    for i in range(m):
-        if i == 0:
-            x_i = a%m
-        else:
-            x_i = (prev**2)%m
-        if bool((b&(2**i))): # Get the ith bit
+    x_i = a%m # x_0 = a%m
+    for i in range(len(bin(b)) - 2): # Because bin returns string '0b{binary number}', we subtract the two characters from it
+        if bool((b&(2**i))): # Get the ith bit from left
             result *= x_i
         prev = x_i
+        x_i = (prev**2)%m # In general, x_i = x_{i-1}^2 (mod m)
     return result%m
 
+if __name__ == '__main__':
+    N = 100003
+    # Generate two random ints such that 2^31 <= a,b < 2^32
+    a = randint(2**31, 2**32-1)
+    b = randint(2**31, 2**32-1)
+
+    print(f"a = {a}\nb = {b}")
+
+    for k, v in {'built-in':pow, 'precomputation': faster_pow, 'memoized divide-and-conque': fast_pow, 'divide-and-conquer': no_memo_pow}.items():
+        print(f"Using {k} algorithm for modular exponentiation.", end="")
+        try:
+            start = time_ns()
+            built_in = v(a, b, N)
+            end = time_ns()
+            print(f" Result: {built_in}. Time elapsed: {end-start} ns")
+        except RecursionError:
+            print(f" Result: recurtionError. Time elapsed: [N/A] ns")
